9793296aa47639751650cf3b87cc5cc5d7768152
Luis Velasco <luisvelasco6541@gmail.com>
2023-07-04 20:33:19 -0500

    Navigation with nested routing

--- lib/widgets/bottom_bar.dart
+++ lib/widgets/bottom_bar.dart
@@ -3,49 +3,52 @@ import 'package:auto_route/auto_route.dart';
 import 'package:hdt_flutter/routers/app_route2.gr.dart';
 
 class BottomBar extends StatelessWidget {
+  const BottomBar({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return AutoTabsRouter(
-      routes: const <AutoRoute>[
-        HomeView(),
-        BuscarView(),
-        FavoritosView(),
-        DeliveryView(),
-      ],
-      builder: (context, child, animation) {
-        return Scaffold(
-          body: FadeTransition(
-            opacity: animation,
-            child: child,
-          ),
-          bottomNavigationBar: BottomNavigationBar(
-            currentIndex: context.tabsRouter.activeIndex,
-            onTap: (index) => context.tabsRouter.setActiveIndex(index),
-            items: const [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                label: 'Inicio',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.search),
-                label: 'Buscar',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.favorite),
-                label: 'Favoritos',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.shopping_cart),
-                label: 'Carrito',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.person),
-                label: 'Perfil',
-              ),
-            ],
-          ),
-        );
-      },
-    );
+    return AutoTabsScaffold(
+        appBarBuilder: (_, tabsRouter) {
+          return AppBar(
+            title: Text(
+              context.tabsRouter.current.name,
+            ),
+            backgroundColor: Colors.transparent,
+            centerTitle: true,
+          );
+        },
+        routes: const [
+          HomeView(),
+          BuscarView(),
+          FavoritosView(),
+          CarritoView(),
+          DeliveryView(),
+        ],
+        bottomNavigationBuilder: (context, tabsRouter) => BottomNavigationBar(
+              currentIndex: context.tabsRouter.activeIndex,
+              onTap: (index) => context.tabsRouter.setActiveIndex(index),
+              items: const [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home),
+                  label: '',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.search),
+                  label: '',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.favorite),
+                  label: '',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.shopping_cart),
+                  label: '',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.moped),
+                  label: '',
+                ),
+              ],
+            ));
   }
 }
